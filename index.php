811fea8473d3813a2d1223c49152fd2901f6b424
voltkigisa <raden.233040043@mail.unpas.ac.id>
2024-03-10 18:18:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
 </head>
 <body>
     <h1>Hello, World!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae iusto culpa quas fugiat odit, facere facilis corrupti quam enim, adipisci eius minima ea eaque similique ipsam at rerum voluptas vitae.
-    </p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae iusto culpa quas fugiat odit, facere facilis corrupti quam enim, adipisci eius minima ea eaque similique ipsam at rerum voluptas vitae.</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
